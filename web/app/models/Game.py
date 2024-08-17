b83bfb7960926955e145e7703d37f5f2ba6b00b5
haydennorris <hayden@mxstore.com.au>
2024-08-17 12:42:19 +1000

    tidy up game to use consts and private status to control how it is updated.

--- web/app/models/Game.py
+++ web/app/models/Game.py
@@ -1,25 +1,58 @@
 from uuid import uuid4
 from app import db
 from app.models.GamePlayer import GamePlayer
-from app.models.Player import Player
-from app.models import BaseModel
+from app.models import BaseModel, Player
 
 
 class Game(BaseModel):
+    STATUS_NEW = 'NEW'
+    STATUS_CLUE_GIVING = 'CLUE_GIVING'
+    STATUS_GUESSING = 'GUESSING'
+    STATUS_FINISHED = 'FINISHED'
+
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(80), nullable=False)
+    _status = db.Column('status', db.String(80), nullable=False)
     code = db.Column(db.String(6), nullable=False)
     players = db.relationship('Player', secondary='game_player', back_populates='games')
 
+    @property
+    def status(self):
+        return self._status
+
+    def set_status(self, value):
+        if value not in [self.STATUS_NEW, self.STATUS_CLUE_GIVING, self.STATUS_GUESSING, self.STATUS_FINISHED]:
+            raise Exception('Invalid status')
+
+        if value == self.status:
+            return self
+
+        if value == self.STATUS_NEW and self.status:
+            raise Exception('Game already started')
+
+        if value == self.STATUS_CLUE_GIVING:
+            if len(self.players) < 2:
+                raise Exception('Invalid status change: Game must have at least 2 players')
+
+            if self.status != self.STATUS_NEW:
+                raise Exception('Invalid status change: Game must be in status NEW to change to CLUE_GIVING')
+
+        # a game can only be marked as guessing if it has a status of 'CLUE_GIVING'
+        if value == self.STATUS_GUESSING and self.status != self.STATUS_CLUE_GIVING:
+            raise Exception('Invalid status change: Game must be in status CLUE_GIVING to change to GUESSING')
+
+        self._status = value
+        return self.save()
+
+
     def __init__(self):
-        self.status = 'NEW'
+        self._status = self.STATUS_NEW
+
 
     @staticmethod
-    def create_game(player: Player, display_name: str = None) -> 'Game':
+    def create(player: Player, display_name: str = None) -> 'Game':
         game = Game()
         game.code = game.__generate_game_code()
-        db.session.add(game)
-        db.session.commit()
+        game.save()
         game.add_player(player, display_name, True)
         return game
 
@@ -27,7 +60,7 @@ class Game(BaseModel):
         code = str(uuid4())[:6]
         # Check if code already exists,
         # we only care if it collides with a game that doesn't have status 'FINISHED'
-        game = Game.query.filter(Game.code == code, Game.status != 'FINISHED').first()
+        game = Game.query.filter(Game.code == code, Game.status != self.STATUS_FINISHED).first()
         if game:
             return self.__generate_game_code()
         return code
@@ -44,7 +77,7 @@ class Game(BaseModel):
     def get_game(game_code: str, status: str = None, active_only: bool = False):
         query = Game.query.filter(Game.code == game_code)
         if active_only:
-            query.filter(Game.status != 'FINISHED')
+            query.filter(Game.status != Game.STATUS_FINISHED)
         if status:
             query.filter(Game.status == status)
         return query.first()
