27e82337a401259428e6a852e3da9acc76d2478a
haydennorris <hayden@mxstore.com.au>
2024-08-30 18:45:23 +1000

    add dotenv support

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from dotenv import load_dotenv  # Import dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 socketio = SocketIO()
