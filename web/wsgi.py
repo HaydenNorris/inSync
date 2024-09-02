e9b96376634ec6baa5951f49c0579c1f86a34c83
haydennorris <hayden@mxstore.com.au>
2024-09-02 16:23:44 +1000

    lets try without providing the path

--- web/wsgi.py
+++ web/wsgi.py
@@ -1,15 +1,12 @@
-import sys
-import os
-
-# Set the path to the directory containing 'app'
-path = os.path.dirname(os.path.abspath(__file__))
-if path not in sys.path:
-    sys.path.append(path)
-
-from app import create_app
+from app import create_app, socketio
 
 # Create the app using the factory function
 app = create_app()
+
+if __name__ == "__main__":
+    import eventlet
+    eventlet.monkey_patch()
+    socketio.run(app, host="0.0.0.0", port=5000)
 
 # The WSGI server looks for a variable named `application`
 application = app
