eedfbfb8c36d7efdfac9d13dce472f6beb8a51fd
haydennorris <hayden@mxstore.com.au>
2024-09-02 16:36:38 +1000

    let's go with the most basic file possible

--- web/wsgi.py
+++ web/wsgi.py
@@ -3,10 +3,5 @@ from app import create_app, socketio
 # Create the app using the factory function
 app = create_app()
 
-if __name__ == "__main__":
-    import eventlet
-    eventlet.monkey_patch()
-    socketio.run(app, host="0.0.0.0", port=5000)
-
 # The WSGI server looks for a variable named `application`
 application = app
